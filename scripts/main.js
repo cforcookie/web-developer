ca8d2a565585b795d45d8c17b0fc3d08a1855c33
flotry <jastbast1103@outlook.com>
2023-04-11 00:16:43 +0300

    Новый сайт

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,6 @@
 'use strict'
 
-
-let container = document.querySelector('.container');
-let items = document.querySelectorAll('.item');
-
-let current = 0;
-
-function next() {
-    current = current + 1;
-
-    if (current > items.length - 1) {
-        current = 0;
-    }
-
-    container.scrollTo({
-        left: items[current].offsetLeft,
-        top: 0,
-        behavior: 'smooth'
-    });
-}
-
-setInterval(next, 3000);
+function copyText() {
+    let copyText = document.getElementById("textToCopy");
+    navigator.clipboard.writeText(copyText.innerText);
+  }
