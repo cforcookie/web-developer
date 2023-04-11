5fdb9befe7c7d2200afc36e59b8aaae0e22419db
flotry <jastbast1103@outlook.com>
2023-04-11 06:33:21 +0300

    another good commit

--- scripts/main.js
+++ scripts/main.js
@@ -23,22 +23,20 @@ function menuPopLeave() {
 }
 
 function scrollToElement(id) {
-    // Найти все ссылки начинающиеся на #
     const anchors = document.querySelectorAll('a[href^="#"]')
 
-    // Цикл по всем ссылкам
     for(let anchor of anchors) {
         anchor.addEventListener("click", function(e) {
             
-        e.preventDefault() // Предотвратить стандартное поведение ссылок
+        e.preventDefault();
 
-        // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
-        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
-        // Плавная прокрутка до элемента с id = href у ссылки
+
+        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body';
+
         document.querySelector(goto).scrollIntoView({
           behavior: "smooth",
           block: "start"
-        })
-    })
-}
+        });
+    });
+};
 }
